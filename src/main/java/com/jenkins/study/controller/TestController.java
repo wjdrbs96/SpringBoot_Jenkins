3e95e59f5973b7107e7196cd6b5c0d23d37315ae
wjdrbs96 <wjdrbs966@naver.com>
2021-03-26 14:07:24 +0900

    Test commit

---
+++ src/main/java/com/jenkins/study/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.jenkins.study.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by jg 2021/03/26
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "jenkins !!!";
+    }
+}
