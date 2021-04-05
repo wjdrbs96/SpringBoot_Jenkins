d80b5274bdf6b629ada51241b384acce77178174
wjdrbs96 <wjdrbs966@naver.com>
2021-04-05 17:28:52 +0900

    update sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,24 @@
-echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
-CURRENT_PID=$(sudo docker container ls -q)
+#!/bin/bash
+BUILD_JAR=$(ls /home/ec2-user/jenkins/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
+
+echo "> build 파일 복사" >> /home/ec2-user/deploy.log
+DEPLOY_PATH=/home/ec2-user/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
 else
-  echo "> sudo docker stop $CURRENT_PID"
-  sudo docker stop $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
-cd /home/ec2-user/jenkins/
-sudo docker build -t wjdrbs96 ./
-sudo docker run -d -p 8080:8080 wjdrbs96  # Docker Container 생성
-sudo docker run -d -p 8081:8080 wjdrbs96  # Docker Container 생성
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
